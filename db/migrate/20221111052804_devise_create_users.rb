11cce3fe9fc80951a59bdfbd8babc0d820105354
EC2 Default User <ec2-user@ip-172-31-34-205.ap-northeast-1.compute.internal>
2022-11-14 15:07:30 +0000

    [update]4-11

--- db/migrate/20221111052804_devise_create_users.rb
+++ db/migrate/20221111052804_devise_create_users.rb
@@ -36,6 +36,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :name
 
       t.timestamps null: false
+
+      t.string :shop_name
+      t.text :coption
+      t.integer :user_id
+
     end
 
     add_index :users, :email,                unique: true
